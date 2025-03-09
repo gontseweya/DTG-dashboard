ffa8f66b3e6e544a905ad02de4cc595ae1b20245
gontseweya <83988312+gontseweya@users.noreply.github.com>
2025-03-10 01:04:25 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 // src/config.ts
 
-export const API_KEY = '589a6d6cedmsh4c0cf365627c871p1156aajsnbebfb7fbb3e5'; // Replace with your actual API key
+export const API_KEY = 'API_KEY'; // Replace with your actual API key
 export const REFRESH_INTERVAL = 60000; // 60 seconds
 export const API_BASE_URL = 'https://alpha-vantage.p.rapidapi.com';
 
